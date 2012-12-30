a8fab04563ddfd4108bb91d88d867444d0196747
Maciej Szeptuch (Neverous) <neverous@neverous.info>
2012-12-30 01:38:35 +0100

    Keep telnet.sh and shell.sh in case of emergency/recovery.

--- hack/shell.sh
+++ hack/shell.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-VALID_PASSWORD=`head -n 1 /data/etc/passwd`
-
 THEPATH='/data/hack/bin:/opt/local/bin:/usr/local/bin:/usr/bin:/bin:/opt/local/sbin:/usr/local/sbin:/usr/sbin:/sbin:/scripts'
 
 for f in /data/plugins/*; do
-	if [ -d ${f}/bin ]; then
-		THEPATH="${f}/bin:${THEPATH}"
-	fi
+    if [ -d ${f}/bin ]; then
+        THEPATH="${f}/bin:${THEPATH}"
+    fi
 done
 
 export PATH="${THEPATH}"
@@ -14,17 +12,6 @@ export LD_LIBRARY_PATH='.:/data/hack/lib:/opt/local/lib:/usr/local/lib:/usr/lib:
 export HOME='/data/hack'
 export ENV='/data/etc/.profile'
 
-read -s -p "Password: " PASSWORD
-
-echo ""
-if [ "$PASSWORD" == "$VALID_PASSWORD" ]; then
-        echo "------------------------"
-        echo " Welcome to Boxee+Hacks "
-        echo "------------------------"
-
-	cd /data/hack
-
-        /bin/sh
-else
-        echo "Incorrect password"       
-fi
+cat /etc/motd
+cd /data/hack
+/bin/sh
